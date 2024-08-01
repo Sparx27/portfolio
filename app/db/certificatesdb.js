a9d46c57249bb7ea9cb6ca826603a46c16fb1821
sparx27 <nicolasgcardani@gmail.com>
2024-08-01 20:05:52 -0300

    Add c# certificate

--- app/db/certificatesdb.js
+++ app/db/certificatesdb.js
@@ -3,12 +3,14 @@ import fccjs from '../../public/images/certificates/fccjs.webp'
 import fccrwd from '../../public/images/certificates/fccrwd.webp'
 import ucss from '../../public/images/certificates/ucss.webp'
 import umern from '../../public/images/certificates/umern.webp'
+import ucsharp from '../../public/images/certificates/ucsharp.png'
 
 
 export const CERTIFICATES_LIST = [
   [escolaridad, 'Average grade'],
   [fccjs, 'FCC JavaScript Certificate'],
   [fccrwd, 'FCC Design Certificate'],
+  [ucsharp, 'U .NET Certificate'],
   [ucss, 'U Web Design Certificate'],
   [umern, 'U MERN Stack Certificate']
 ]
