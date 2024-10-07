654e33386699803d73884ac583fbd032356001e9
sparx27 <nicolasgcardani@gmail.com>
2024-10-07 12:53:28 -0300

    Add First Certificate in English

--- app/db/certificatesdb.js
+++ app/db/certificatesdb.js
@@ -1,4 +1,5 @@
 import escolaridad from '../../public/images/certificates/escolaridad.png'
+import firstcertificate from '../../public/images/certificates/firstcertificate.jpg'
 import fccjs from '../../public/images/certificates/fccjs.webp'
 import fccrwd from '../../public/images/certificates/fccrwd.webp'
 import ucss from '../../public/images/certificates/ucss.webp'
@@ -9,6 +10,7 @@ import apicsharp from '../../public/images/certificates/apicsharp.png'
 
 export const CERTIFICATES_LIST = [
   [escolaridad, 'Average grade'],
+  [firstcertificate, 'First Certificate in English'],
   [apicsharp, 'U .NET Web API Certificate'],
   [ucsharp, 'U .NET Certificate'],
   [ucss, 'U Web Design Certificate'],
