5fec68c0c717df2db038ab99dacf3e80be4d6218
sparx27 <nicolasgcardani@gmail.com>
2024-10-14 15:04:37 -0300

    minor footer change

--- app/db/textEs.js
+++ app/db/textEs.js
@@ -28,7 +28,7 @@ export const es = {
       'A principios de 2022, comenzó mi viaje en la programación, cuando me topé con algo que realmente despertó mi interés: un curso en línea titulado "JavaScript Algorithms and Data Structures" en freecodecamp.com. Desde entonces, he estado avanzando continuamente en mis conocimientos en desarrollo web, a través de la lectura y puesta en práctica de documentación y participando en cursos en línea de distintas plataformas. Además, <span class="text-decoration-underline">comencé mis estudios formales en la Universidad ORT Uruguay en agosto de 2023, cursando la carrera "Analista en Tecnologías de la Información"</span>. Actualmente continúo expandiendo de forma independiente mis conocimientos en programación mientras curso mis estudios formales.',
       'Tengo 29 años, nací en los Estados Unidos y vine a vivir a Uruguay cuando era niño. Ahora resido en Montevideo. Mis intereses son diversos e incluyen el deporte, la filosofía, la psicología y las artes visuales. Soy una persona responsable, me gustan las actividades en grupo y disfruto mucho de aprender tecnologías y técnicas nuevas para programar.'
     ],
-    portfolio: 'Hecho con gusto en Remix Run. Código del Portfolio:'
+    portfolio: 'Hecho con dedicación en Remix Run. Código del Portfolio:'
   },
   projects: {
     t1: 'Proyectos',
