77837ba845fd372a209dfb4493a9be05612ac3a3
sparx27 <nicolasgcardani@gmail.com>
2024-10-15 10:34:26 -0300

    update

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -72,7 +72,7 @@ function Header({ display }) {
                 <div className='contact-container'>
                   <div className='contact-container'>
                     <div className='linkedin-box'>
-                      <a href='https://www.linkedin.com/in/nicol%C3%A1s-gim%C3%A9nez-cardani-68429b230/' target='_blank' rel='noreferrer'>
+                      <a href='https://www.linkedin.com/in/gcnicolas2024/' target='_blank' rel='noreferrer'>
                         <BsLinkedin className='contact-icon linkedin' />
                       </a>
                     </div>
